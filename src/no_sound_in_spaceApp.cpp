8b09cae94be4a5255259e7b2841a2276c7755704
drichter42 <david.richter42@gmail.com>
2017-01-23 21:32:13 -0800

    rotating sun

--- src/no_sound_in_spaceApp.cpp
+++ src/no_sound_in_spaceApp.cpp
@@ -1,6 +1,7 @@
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
+#include "cinder/Easing.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -12,10 +13,27 @@ class no_sound_in_spaceApp : public App {
 	void mouseDown( MouseEvent event ) override;
 	void update() override;
 	void draw() override;
+    
+    CameraPersp mCam;
+    // gl::BatchRef mSphere;
+    gl::BatchRef mShape;
+    
+    float theta;
 };
 
 void no_sound_in_spaceApp::setup()
 {
+    auto lambert = gl::ShaderDef().lambert().color();
+    gl::GlslProgRef shader = gl::getStockShader( lambert );
+    // mCam.lookAt( vec3( 3, 3, 3 ), vec3( 0 ) );
+    
+    // auto sphere = geom::Sphere().subdivisions( 30 );
+    // mSphere = gl::Batch::create( sphere, shader );
+    auto icosahedron = geom::Icosahedron();
+    mShape = gl::Batch::create( icosahedron, shader );
+    
+    theta = 0;
+    
 }
 
 void no_sound_in_spaceApp::mouseDown( MouseEvent event )
@@ -24,11 +42,29 @@ void no_sound_in_spaceApp::mouseDown( MouseEvent event )
 
 void no_sound_in_spaceApp::update()
 {
+    theta += 0.01;
 }
 
 void no_sound_in_spaceApp::draw()
-{
+{   
+    // .lookAt( vec3(&eyepoint), vec3( target ) )
+    mCam.lookAt( vec3( 3, 0, 10 ), vec3( 0 ) );
+    
+    
+    // This does some neat stuff
+    // float angle = easeInOutQuint( theta ) * M_PI / 2.0f;
+    
 	gl::clear( Color( 0, 0, 0 ) ); 
+    gl::enableDepthRead();
+    gl::enableDepthWrite();
+    
+    gl::setMatrices( mCam );
+    
+    gl::color( 0.2, 0.7, 0.2 );
+    
+    gl::rotate( angleAxis( theta, vec3( 0, 1, 0 ) ) );
+    // mSphere->draw();
+    mShape->draw();
 }
 
 CINDER_APP( no_sound_in_spaceApp, RendererGl )
---
+++ xcode/build/no_sound_in_space.build/Debug/no_sound_in_space.build/Objects-normal/x86_64/no_sound_in_spaceApp.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/davidrichter/src/cpp/cinder_0.9.0_mac/apps/no_sound_in_space/src/no_sound_in_spaceApp.cpp \
+  ../../../include/cinder/app/RendererGl.h \
+  ../../../include/cinder/Easing.h
